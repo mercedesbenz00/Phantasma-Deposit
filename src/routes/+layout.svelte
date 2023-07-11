db85275793d5163984bd7c2cc2f8c4d2f609ebf8
mercedesbenz00 <sony010521@gmail.com>
2023-07-11 12:21:54 +0100

    Fixing the Name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import '../app.css';
+	import '../app.css';
 	import '../soft-ui-dashboard-tailwind.min.css';
-    import Header from '$lib/Components/Header/Header.svelte';
+	import Header from '$lib/Components/Header/Header.svelte';
 	import Footer from '$lib/Components/Footer/Footer.svelte';
 	import Sidebar from '$lib/Components/Sidebar/Sidebar.svelte';
 	import ConnectWalletModal from '$lib/Components/Wallet/ConnectWalletModal.svelte';
-    import { SvelteToast } from '@zerodevx/svelte-toast';
+	import { SvelteToast } from '@zerodevx/svelte-toast';
 	import { NotificationsOptions } from '$lib/Components/Notification/NotificationsBuilder';
-	import { connectWallet, OpenedModal, PhantasmaAPIClient } from '$lib/store';
+	import { connectWallet, OpenedModal, PhantasmaAPIClient, SoftwareName } from '$lib/store';
 	import { ModalInternalTypes } from '$lib/Components/Modals/ModalInternalTypes';
 
-    let showConnectWalletModal: boolean;
+	let showConnectWalletModal: boolean;
 	let hash = '';
 	connectWallet.subscribe((value) => {
 		showConnectWalletModal = value;
@@ -28,20 +28,18 @@
 	OpenedModal.subscribe((value) => {
 		openedModal = value;
 	});
-
 </script>
 
 <svelte:head>
-	<title>Phantasma Claim</title>
+	<title>{SoftwareName}</title>
 </svelte:head>
 
 <Sidebar />
 <main
 	class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200"
 >
-
-    <Header />
-    <div class="mb-4 px-6 mx-auto">
+	<Header />
+	<div class="mb-4 px-6 mx-auto">
 		<div class="flex flex-wrap mt-6 -mx-3">
 			{#if showConnectWalletModal}
 				<ConnectWalletModal on:close={() => closeConnectWallet()} />
@@ -49,18 +47,16 @@
 		</div>
 	</div>
 
-    <slot />
-
+	<slot />
 </main>
 
- <Footer />
+<Footer />
 
+<SvelteToast options={NotificationsOptions} />
 
- <SvelteToast options={NotificationsOptions} />
-
- <style>
-     :root {
-         --toastContainerBottom: 4rem;
-         --toastContainerTop: auto;
-     }
- </style>
+<style>
+	:root {
+		--toastContainerBottom: 4rem;
+		--toastContainerTop: auto;
+	}
+</style>
